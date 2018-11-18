d35ac34a7a80c63712c372ae5b40122d43a04cb9
Bodya Lyzen <43245185+BodyaLyz@users.noreply.github.com>
2018-11-18 15:00:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,48 +2,51 @@
 
 var ua = 26;
 
-function convertUA () {
-	var usa = document.getElementById('convert').value;
-	result = usa * ua;
-	document.getElementById("res").innerHTML = "Your money ua:"+"  "+ result;
+function convertToUa() {
+    var usa = document.getElementById('convert').value;
+    result = usa * ua;
+    document.getElementById("res").innerHTML = "Your money ua:" + "  " + result;
 }
 
-function convertUSA () {
-	var usa = document.getElementById('converttwo').value;
-	result = usa / ua;
-	document.getElementById("res").innerHTML = "Your money usa:"+"  "+ result;
+function convertToUsd() {
+    var usa = document.getElementById('converttwo').value;
+    result = usa / ua;
+    document.getElementById("res").innerHTML = "Your money usa:" + "  " + result;
 }
 
 // task one - 2
 
-function findMinMax() {
-    document.getElementById("result").innerHTML = "max " + Math.max(1,23,-5,11) + ", min " + Math.min(1,23,-5,11);
+var given = [1, 23, -5, 11];
+
+function findMinMax(arr) {
+    document.getElementById("result").innerHTML = "max " + Math.max(...arr) + ", min " + Math.min(...arr);
 }
 
-// task one-3 
+findMinMax(given)
 
-var elmt = [1,4,2];
+// task one - 3 
 
-function average() {
-	var sum = 0;	
-	for( var i = 0; i < elmt.length; i++ ){
-    sum += parseInt( elmt[i] ); 
-	var avg = sum/elmt.length;
-		
-document.getElementById('res2').innerHTML = "Сума:"+' '+ sum + "<br> Cереднє значення:"+" "+ avg.toFixed(2);
+var elmt = [1, 4, 2];
+
+function average(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += parseInt(arr[i]);
+        var avg = sum / arr.length;
+
+        document.getElementById('res2').innerHTML = "Сума:" + ' ' + sum + "<br> Cереднє значення:" + " " + avg.toFixed(2);
+    }
 }
-}
+
+average(elmt);
 
 // task two
 
-function recursion(num) {
-    if(num >= 1) {
-    document.write( ' ' + num);
+function recursion(n) {
+    if (n >= 1) {
+        document.write(' ' + (n));
+        recursion(n - 1);
+    }
 }
 recursion(10)
-
-
-
-
-
 
